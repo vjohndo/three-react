04c267afbbbad71ea9cd8c792094e41e828a01ba
John Do <v.john.do@outlook.com>
2021-12-11 21:46:19 +1100

    practiced shadows and lighting

--- threejs-journey/module_01/13-3d-text/src/script.js
+++ threejs-journey/module_01/13-3d-text/src/script.js
@@ -69,9 +69,9 @@ fontLoader.load(
         for (let i = 0; i < 100; i++) {
             const donut = new THREE.Mesh(donutGeometry, donutMaterial)
 
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
+            donut.position.x = (Math.sin(Math.random()) - 0.5) * 10
+            donut.position.y = (Math.sin(Math.random()) - 0.5) * 10
+            donut.position.z = (Math.sin(Math.random()) - 0.5) * 10
             
             donut.rotation.x = Math.random() * Math.PI
             donut.rotation.y = Math.random() * Math.PI
